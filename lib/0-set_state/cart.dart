10ec1c3b221e9dba9a97e415e00b90664c74323f
daehooo <eoho15@gmail.com>
2025-05-14 15:47:04 +0900

    setState를 이용한 방법을 main으로 병합하기 위한 코드

---
+++ lib/0-set_state/cart.dart
@@ -0,0 +1,40 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_practice/common/product.dart';
+import 'package:flutter_practice/common/product_tile.dart';
+
+class Cart extends StatelessWidget {
+  const Cart({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Product> cartProductList = [];
+
+    return Scaffold(
+      body: cartProductList.isEmpty
+
+          /// Empty
+          ? const Center(
+              child: Text(
+                "Empty",
+                style: TextStyle(fontSize: 24, color: Colors.grey),
+              ),
+            )
+
+          /// Not Empty
+          : ListView.builder(
+              itemCount: cartProductList.length,
+              itemBuilder: (context, index) {
+                Product product = cartProductList[index];
+                return ProductTile(
+                  product: product,
+                  isInCart: true,
+                  onPressed: (product) {},
+                );
+              },
+            ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter_practice/1-stateful_widget/home_page.dart';
+
+import '0-set_state/home_page.dart';
 
 void main() {
   runApp(const MyApp());
